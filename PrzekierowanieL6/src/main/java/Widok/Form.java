cf26778381c212a597f44c9176102b066bbea90b
PatrykJaseniuk <patryk.jaseniuk@gmail.com>
2022-01-03 19:11:05 +0100

    L6

---
+++ PrzekierowanieL6/src/main/java/Widok/Form.java
@@ -0,0 +1,34 @@
+package Widok;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jasyn
+ */
+public class Form extends Element {
+
+    private String action;
+
+    public Form(String action) {
+        this.action = action;
+    }
+
+    @Override
+    public String getHtml() {
+        String html = "<form  method=\"post\" enctype=\"multipart/form-data\" action=\"" + action + "\">\n";
+
+        for (Element element : elementy) {
+            html += element.getHtml();
+        }
+
+        html
+                += "  <input type=\"submit\" value=\"Submit\">\n"
+                + "</form> ";
+
+        return html;
+    }
+}
